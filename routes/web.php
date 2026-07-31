e2f6ce5d6d8236481be1e47896446c665a3c5085
Andres-Mt <ayiviabravi2@gmail.com>
2026-07-31 20:53:08 +0000

    Ajout route temporaire pour migration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-migrations-secret', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return "Migrations exécutées avec succès.";
+});
