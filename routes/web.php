da1396e369c65aeb840f800715af8fb2f8e9fafa
Andres-Mt <ayiviabravi2@gmail.com>
2026-07-31 21:46:33 +0000

    Ajout route pour seed

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::get('/run-migrations-secret', function () {
     \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
     return "Migrations exécutées avec succès.";
 });
+
+Route::get('/run-seed-secret', function () {
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+    return "Données de test (Seeders) injectées avec succès.";
+});
